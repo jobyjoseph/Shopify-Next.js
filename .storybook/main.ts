8dfcedca0443fb10bbb2e704fb01ee456c64c36a
Joby Joseph <reachjoby@gmail.com>
2024-02-03 18:20:05 -0800

    build: implement storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,10 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  /** Expose public folder to storybook as static */
-  staticDirs: ['../public'],
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
